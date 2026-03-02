6ae3332e17a3c15c6e5490ebba6d93597c14fe18
Gowsalya <gowsalyashalo7@gmail.com>
2026-03-02 21:28:38 +0530

    Update 001_top_song.sql

--- Easy/001_top_song.sql
+++ Easy/001_top_song.sql
@@ -1,2 +1,24 @@
-select business_name, review_text from yelp_reviews 
-where cool = (select Max(cool) from yelp_reviews)
+/*
+Problem: Top Ranked Songs
+Platform: StrataScratch
+Difficulty: Easy
+
+Problem Summary:
+Find the song(s) that appeared in the top position (position = 1)
+the highest number of times.
+Return results in descending order of appearances.
+
+Approach:
+1. Filter rows where position = 1.
+2. Group by trackname.
+3. Count the number of occurrences.
+4. Order the result by count in descending order.
+*/
+
+SELECT 
+    trackname, 
+    COUNT(*) AS times_top1
+FROM spotify_worldwide_daily_song_ranking
+WHERE position = 1
+GROUP BY trackname
+ORDER BY times_top1 DESC;
